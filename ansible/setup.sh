0e115c039a2f677a2ae6cb583d7dba6527534f0f
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-09-05 18:15:46 +0300

    Update setup.sh

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -2,4 +2,4 @@
 sudo -S yum install python -y
 sudo -S yum install python-pip -y
 sudo -S pip install ansible
-ansible-playbook -i inventory.ini home/ec2-user/ansible/ansible/my_playbook.yml
+ansible-playbook -i inventory.ini /home/ec2-user/ansible/ansible/my_playbook.yml
